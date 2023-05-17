5c6432763ed7da5f2b431b9588ab534d1a785371
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-05-17 14:17:49 +0200

    add eslint file, fix problems in js thrown by eslint, change post styles in css

--- js/script.js
+++ js/script.js
@@ -1,75 +1,76 @@
-'use strict'
+'use strict';
 
 {
-	const titleClickHandler = function (event) {
-		event.preventDefault()
+  const titleClickHandler = function (event) {
+    event.preventDefault();
 
-		const clickedElement = this
+    const clickedElement = this;
 
-		/* [DONE] remove class 'active' from all article links  */
-		const activeLinks = document.querySelectorAll('.titles a.active')
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-		for (let activeLink of activeLinks) {
-			activeLink.classList.remove('active')
-		}
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }
 
-		/* [DONE] add class 'active' to the clicked link */
-		clickedElement.classList.add('active')
-		// console.log(activeLinks);
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    // console.log(activeLinks);
 
-		/* [DONE] remove class 'active' from all articles */
-		const activeArticles = document.querySelectorAll('article.active')
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('article.active');
 
-		for (let activeArticle of activeArticles) {
-			activeArticle.classList.remove('active')
-		}
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
 
-		/* [DONE] get 'href' attribute from the clicked link */
-		const hrefClickedLink = clickedElement.getAttribute('href')
+    /* [DONE] get 'href' attribute from the clicked link */
+    const hrefClickedLink = clickedElement.getAttribute('href');
 
-		/* [DONE] find the correct article using the selector (value of 'href' attribute) */
-		const articleToShow = document.querySelector(`article${hrefClickedLink}`)
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const articleToShow = document.querySelector(`article${hrefClickedLink}`);
 
-		/* [DONE] add class 'active' to the correct article */
-		articleToShow.classList.add('active')
-	}
+    /* [DONE] add class 'active' to the correct article */
+    articleToShow.classList.add('active');
+  };
 
-	const optArticleSelector = '.post'
-	const optTitleSelector = '.post-title'
-	const optTitleListSelector = '.titles'
+  const optArticleSelector = '.post';
+  const optTitleSelector = '.post-title';
+  const optTitleListSelector = '.titles';
 
-	const generateTitleLinks = function () {
-		// [DONE] remove contents of titleList
-		const titleList = (document.querySelector(optTitleListSelector).innerHTML = '')
+  const generateTitleLinks = function () {
+    // [DONE] remove contents of titleList
 
-		// for each article:
-		//  [DONE] get id and store it to const
-		const articles = document.querySelectorAll(optArticleSelector)
+    document.querySelector(optTitleListSelector).innerHTML = '';
 
-		let html = ''
-		for (let article of articles) {
-			// 	[DONE]const articleID = article.id
-			const articleID = article.id
+    // for each article:
+    //  [DONE] get id and store it to const
+    const articles = document.querySelectorAll(optArticleSelector);
 
-			// [DONE] find element with title and store it to const
-			const articleTitle = article.querySelector(optTitleSelector).innerHTML
+    let html = '';
+    for (let article of articles) {
+      // 	[DONE]const articleID = article.id
+      const articleID = article.id;
 
-			// [DONE] generate HTML code and store it to const,
-			const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>'
-			html = html + linkHTML
-		}
+      // [DONE] find element with title and store it to const
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-		// [DONE] insert generated code to left column
-		const ulList = document.querySelector('.titles')
-		ulList.insertAdjacentHTML('afterbegin', html)
+      // [DONE] generate HTML code and store it to const,
+      const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+      html = html + linkHTML;
+    }
 
-		const links = document.querySelectorAll('.titles a')
-		links[0].classList.add('active')
+    // [DONE] insert generated code to left column
+    const ulList = document.querySelector('.titles');
+    ulList.insertAdjacentHTML('afterbegin', html);
 
-		for (let link of links) {
-			link.addEventListener('click', titleClickHandler)
-		}
-	}
+    const links = document.querySelectorAll('.titles a');
+    links[0].classList.add('active');
 
-	generateTitleLinks()
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
+  };
+
+  generateTitleLinks();
 }
